6541ce13e2134ea0f6fe4bd946499d7c67f674f3
forestai123456 <jianguodai30@gmail.com>
2026-01-21 06:39:56 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "wrs-cache-v3";
+﻿const CACHE_NAME = "wrs-cache-v4";
 const ASSETS = [
   "./",
   "./index.html",
@@ -33,5 +33,6 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
+
 
 
